550798d8c860eb1003065cb8fbd2c334ee799f1d
Leif Gruenwoldt <leifer@gmail.com>
2017-01-15 13:25:24 -0500

    Use Overpass API instead
    This API is meant more for fetching data. The other for editing.

--- example-request.sh
+++ example-request.sh
@@ -1,1 +1,46 @@
-curl http://api.openstreetmap.org/api/0.6/map?bbox=11.54,48.14,11.543,48.145
+#!/bin/bash
+#
+# Example demonstrating the use of OSM Overpass API to request data for a golf GPS.
+
+
+overpassApiUrl="http://overpass-api.de"
+
+my_lat=44.2450803
+my_lon=-76.4488947
+
+run_query() {
+    query=$1
+    overpassQuery=$overpassApiUrl"/api/interpreter?data=\[out:json\];"$query
+    echo $overpassQuery
+    curl $overpassQuery
+}
+
+get_nearby_golfcourses() {
+    lat=$1
+    lon=$2
+    query="way(around:100.0,"$lat","$lon")\[leisure=golf_course\];out%20tags;"
+    echo $query
+}
+
+get_nearby_hole() {
+    golfcourse_name=$1
+    # TODO
+}
+
+get_distance_to_pin() {
+    golfcourse_name=$1
+    hole_number=$2
+    # TODO
+}
+
+# 1. Confirm the golf course you are at.
+query=`get_nearby_golfcourses $my_lat $my_lon`
+run_query $query
+
+# 2. Confirm the hole you are on.
+query=`get_nearby_hole`
+run_query $query
+
+# 3. How far are you from the pin.
+query=`get_distance_to_pin`
+run_query $query
