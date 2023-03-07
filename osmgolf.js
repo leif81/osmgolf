2340bc0c7072a03cfdf1efb9bb6bda63d7ecad53
Leif Gruenwoldt <leifer@gmail.com>
2023-03-06 22:50:07 -0500

    Replace bash example with html/js app example

---
+++ osmgolf.js
@@ -0,0 +1,102 @@
+/* jshint browser: true, devel: true */
+/* global async, Map, Set */
+'use strict';
+
+// Override these by passing in an object with any of these key/value pairs into the initGitLabPb function (e.g. from gitlabpb.html page).
+// The privateToken requires read_api scope.
+var osmGolfOptions = {
+    domElement: "#osmgolf",
+    service: "http://overpass-api.de/api/interpreter?data=\[out:json\];",
+    checkForUpdates: false
+};
+
+function initOsmGolf(options) {
+    osmGolfOptions = Object.assign(osmGolfOptions, options); // Merge custom options into defaults.
+    loadParams();
+    var app = createOsmGolf()
+    document.querySelector(osmGolfOptions.domElement).appendChild(app);
+    getNearestGolfCourse();
+    console.log("osmgolf loaded!");
+}
+
+function createOsmGolf() {
+    var osmgolf = document.createElement("div");
+    return osmgolf;
+}
+
+function getCurrentLocation() {
+    // TODO - in future request location from browser API.
+    return {lat: 44.2450803, lon: -76.4488947 }; // This position is at Garrison Golf Course south of the pro shop.
+}
+
+function getNearestGolfCourse() {
+    var myLocation = getCurrentLocation();
+    var request = osmGolfOptions.service + "way(around:100.0," + myLocation.lat + "," + myLocation.lon + ")\[leisure=golf_course\];out%20tags;";
+    
+    httpGet(request, function(response) {
+        response.elements.forEach(element => {
+            var courseName = document.createTextNode("The nearest golf course is " + element.tags.name + ".");
+            document.querySelector(osmGolfOptions.domElement).appendChild(courseName);
+        });
+    });
+}
+
+function getNearestHole() {
+    // TODO
+}
+
+function getDistanceToPin() {
+    //TODO
+}
+
+// Load optional configuration parameters from the browser client URL.
+// Any parameters found will override the statically configured options in osmGolfOptions.
+function loadParams() {
+
+    var autorefresh = getURLParameter("autorefresh");
+    if (autorefresh !== null) {
+        osmGolfOptions.autoRefresh = isTrue(autorefresh);
+    }
+}
+
+function httpGet(url, successCallback, errorCallback) {
+	if (url.indexOf("?") == -1) {
+	    url += "?";
+	} else {
+	    url += "&";
+	}
+
+    fetch(url).then(function(response) {
+            return response.json()
+        }).then(function(data) {
+            return successCallback(data)
+        }).catch(function(error) {
+            console.error('Error:', error);
+        });
+}
+
+function getURLParameter(parameter) {
+    return decodeURIComponent((new RegExp('[?|&]' + parameter + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+}
+
+function scheduleCheckForUpdates() {
+    window.setTimeout(function() {
+        loadCheckForUpdates();
+    }, 600000);
+}
+
+function isTrue(string) {
+    return (string === 'true');
+}
+
+// Register event handlers
+
+// Background checking for updates to visible cards
+document.addEventListener("visibilitychange", function() {
+    if (document.hidden) {
+        osmGolfOptions.checkForUpdates = false;
+    } else {
+        osmGolfOptions.checkForUpdates = true;
+        //loadCheckForUpdates();
+    }
+});
